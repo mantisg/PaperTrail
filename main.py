3644a8938137dd65d37e04e8812626ad03aa1f23
mantisg <joshuabliven93@gmail.com>
2026-01-06 00:24:27 -0500

    Initial window and movement setup

---
+++ main.py
@@ -0,0 +1,43 @@
+import pygame
+import os
+import random
+import time
+import tkinter as tk
+pygame.font.init()
+
+root = tk.Tk()
+WIDTH = root.winfo_screenwidth()
+HEIGHT = root.winfo_screenheight()
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+pygame.display.set_caption("PaperTrail: Escape from this Dimension")
+running = True
+dt = 0.1
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((150, 150, 150))
+    pygame.draw.lines(screen, "blue", False, [(0, 0), (WIDTH, 0), (WIDTH, HEIGHT), (0, HEIGHT)], 5)
+
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
+    
+    pygame.display.flip()
+    clock.tick(60) / 1000
+pygame.quit()
